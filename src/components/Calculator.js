b0e53e4d33eddc02db8320db454523fef73864b3
SViron00 <vironsean31@gmail.com>
2025-02-05 15:10:54 +0000

    container layout

---
+++ src/components/Calculator.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Calculator = () => {
+  return (
+    <div className="calculator-container">
+      <div className="calculator-wrapper">
+        <div className="display-placeholder"></div>
+
+        <div className="keypad-placeholder"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
